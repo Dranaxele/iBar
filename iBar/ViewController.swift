a62de22391eebcc30af552eeede0cac877eabf90
Alexandre Plaitant <Alexandre@pc12.home>
2015-03-23 15:48:08 +0100

    Initial Commit

---
+++ iBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBar
+//
+//  Created by Alexandre Plaitant on 23/03/2015.
+//  Copyright (c) 2015 Alexandre Plaitant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
