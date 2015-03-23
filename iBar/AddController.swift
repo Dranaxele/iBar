ded9a7423cf4e8fb8c0ce72d20485767fbd9607f
Alexandre Plaitant <Alexandre@pc12.home>
2015-03-23 19:04:09 +0100

    Init

---
+++ iBar/AddController.swift
@@ -0,0 +1,40 @@
+//
+//  AddController.swift
+//  iBar
+//
+//  Created by Alexandre Plaitant on 23/03/2015.
+//  Copyright (c) 2015 Alexandre Plaitant. All rights reserved.
+//
+
+import UIKit
+
+class AddController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func SaveButton(sender: AnyObject) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
